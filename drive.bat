b07b60e0d924ba67a52b72fc023fffa98659c1ed
EFS777 <efs7777777@gmail.com>
2025-06-28 00:13:33 +0530

    bat file added

--- drive.bat
+++ drive.bat
@@ -1,6 +1,13 @@
 @echo off
 echo Starting Server...
 start cmd /k "cd server && node server.js"
-echo Starting drive...
-start cmd /k "cd drive && npm start"
-echo Drive is started
+
+:WAIT
+powershell -command ^
+  "$r = Invoke-WebRequest -Uri http://localhost:3001 -UseBasicParsing -TimeoutSec 1; if ($r.StatusCode -eq 200) { exit 0 }" >nul 2>&1
+if errorlevel 1 (
+    timeout /t 1 >nul
+    goto WAIT
+)
+
+start http://localhost:3001
