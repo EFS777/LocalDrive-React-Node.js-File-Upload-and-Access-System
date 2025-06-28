7cfd7bda5f82dacf2b7c30e72a8d16790385b71d
EFS777 <efs7777777@gmail.com>
2025-06-28 15:21:09 +0530

    changes

--- drive/src/components/modal/modal.js
+++ drive/src/components/modal/modal.js
@@ -18,11 +18,8 @@ export default function Modal({ trigger, element, inital = false, callback }) {
         }
         {modal &&
             <section className="modal" onClick={Close}>
-                <IoMdCloseCircleOutline size={27} className="m_close" onClick={Close} />
+                <IoMdCloseCircleOutline size={27} className="close" onClick={Close} />
                 <div className="modal-body" onClick={(e)=>e.stopPropagation()}>
-                    <IoMdCloseCircleOutline size={27} className="d_close" onClick={() => {
-                        setModal(false);
-                    }} />
                     {element}
                 </div>
             </section>}
